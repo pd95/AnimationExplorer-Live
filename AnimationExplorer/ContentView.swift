3328a67a6d592f8b4fff0140c035a95d64249a78
Philipp <pd95@users.noreply.github.com>
2023-06-24 18:12:53 +0200

    Initial Commit

---
+++ AnimationExplorer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AnimationExplorer
+//
+//  Created by Philipp on 24.06.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
